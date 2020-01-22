742470c46f17231c56771863f9d32de9e671e724
Maria Luiza Coelho <co.maria.luiza@gmail.com>
2020-01-22 08:11:27 -0300

    changed listen process on app

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.use('/', indexRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 app.use('/campgrounds', campgroundRoutes);
 
-app.listen(3000, function() {
+/* app.listen(3000, function() {
     console.log('App running on port 3000.');
-});
+}); */
 
-/* app.listen(process.env.PORT || 5000); */
+app.listen(process.env.PORT || 5000);
